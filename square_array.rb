da3b87843a1b943e5745dc7a9251d3d741e240a4
Alison Shin <alisonyshin@gmail.com>
2018-07-30 02:23:45 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,14 @@
 numbers = [1,2,3]
 
 def square_array(array)
-  new_numbers = array.collect {|number| number*number}
+  new_numbers=[]
+  array.collect {|number| number*number}
+    new_number = number * number
+    new_numbers.push(new_number)
+  end
+  return new_numbers
 end
+
+(1..4).collect { |i| i*i }
+
+array.collect {|number| number*number}
