533a8619e51884626662eec65f8f3b03b0f13782
Alison Shin <alisonyshin@gmail.com>
2018-07-30 02:24:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ numbers = [1,2,3]
 
 def square_array(array)
   new_numbers=[]
-  array.collect {|number| number*number}
+  array.each {|number| new_numbers << number*number}
     new_number = number * number
     new_numbers.push(new_number)
   end
@@ -12,3 +12,9 @@ end
 (1..4).collect { |i| i*i }
 
 array.collect {|number| number*number}
+
+def square_array(array)
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
+end
